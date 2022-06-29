1eafc576ffa3cefcc84584218f88a6a016826ee6
Rameez <rameez0khawaja@gmail.com>
2022-06-29 11:54:13 +0100

    heroku port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require("./app")
 
-const port = 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
